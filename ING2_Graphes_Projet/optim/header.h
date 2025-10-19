2f5dc6cc6ec1cb385f20222b1140014e14ad73b0
MaloLab <malocolincreation@gmail.com>
2025-10-20 00:14:29 +0200

    Add files via upload

---
+++ ING2_Graphes_Projet/optim/header.h
@@ -0,0 +1,28 @@
+//
+// Created by maloc on 19/09/2024.
+//
+
+#ifndef OPTIM_HEADER_H
+#define OPTIM_HEADER_H
+
+
+
+///1. BIBLIOTHEQUES
+//INCLUDE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <conio.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <windows.h>
+
+
+//interface
+void premierpage();
+void color(int couleurDuTexte,int couleurDeFond);
+void voiture();
+
+
+#endif //OPTIM_HEADER_H
---
+++ ING2_Graphes_Projet/optimisation-d-une-ligne-d-assemblage-ing2_2023-2024_td9_grp9/header.h
@@ -0,0 +1,26 @@
+// header.h
+
+#ifndef TEMPS_PRECEDENCE_PROJ_HEADER_H
+#define TEMPS_PRECEDENCE_PROJ_HEADER_H
+
+
+
+///1. BIBLIOTHEQUES
+//INCLUDE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <conio.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <windows.h>
+
+
+//interface
+void premierpage();
+void color(int couleurDuTexte,int couleurDeFond);
+void voiture();
+
+
+#endif // TEMPS_PRECEDENCE_PROJ_HEADER_H
